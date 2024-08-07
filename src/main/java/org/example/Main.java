c7e4198fe9757cef2976c4f8b8bf6383b9edcb52
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-08-07 21:09:27 +0300

    tasks done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,76 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Employee> employees = new LinkedList<>();
+
+        Employee employee1 = new Employee(1, "Furkan", "Kızılbuğa");
+        Employee employee2 = new Employee(1, "Furkan", "Kızılbuğa");
+        Employee employee3 = new Employee(3, "Gracie", "Abrams");
+        Employee employee4 = new Employee(3, "Gracie", "Abrams");
+        Employee employee5 = new Employee(3, "Gracie", "Abrams");
+        Employee employee6 = new Employee(4, "Hayley", "Williams");
+
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee4);
+        employees.add(employee5);
+        employees.add(employee6);
+
+        System.out.println("Duplicates: " + findDuplicates(employees));
+        System.out.println("Uniques: " + findUniques(employees));
+        System.out.println("Removed: " + removeDuplicates(employees));
+
+        System.out.println("Word Count: " + WordCounter.calculatedWord());
+
     }
+
+    public static List<Employee> findDuplicates(List<Employee> employees) {
+        List<Employee> duplicates = new LinkedList<>();
+
+        for(int i = 0; i < employees.size(); i++) {
+            if(employees.get(i) != null && employees.get(i).getId() != null) {
+                Integer employee = employees.get(i).getId();
+                if(employee == null) return duplicates;
+                for (int j = i + 1; j < employees.size(); j++) {
+                    if (employees.get(j) != null && employees.get(j).getId() != null && employee.equals(employees.get(j).getId()) && !duplicates.contains(employees.get(j))) {
+                        duplicates.add(employees.get(i));
+                    }
+                }
+            }
+        }
+        return duplicates;
+    }
+
+    public static Map<Integer, Employee> findUniques(List<Employee> employees) {
+        Map<Integer, Employee> uniques = new HashMap<>();
+
+        for(Employee employee : employees) {
+            if(employee != null) uniques.put(employee.getId(), employee);
+        }
+
+        return uniques;
+    }
+
+    public static List<Employee> removeDuplicates(List<Employee> employees) {
+        List<Employee> uniques = new ArrayList<>();
+
+        List<Employee> duplicates = findDuplicates(employees);
+
+        for (Employee employee : employees) {
+            if(!duplicates.contains(employee) && employee != null) {
+                    uniques.add(employee);
+            }
+        }
+
+        return uniques;
+    }
+
 }
---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,50 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(Integer id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstname + '\'' +
+                ", lastName='" + lastname + '\'' +
+                '}';
+    }
+
+}
